b4cb727044f83ebddf9511efc25048a3d0c04302
petrsidann <petrsidann@gmail.com>
2026-08-22 16:52:56 +0300

    MainActivity.kt

---
+++ app/src/main/java/com/bingwascore/app/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.bingwascore.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.bingwascore.app.ui.navigation.BingwaNavHost
+import com.bingwascore.app.ui.theme.BingwaTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BingwaTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    BingwaNavHost()
+                }
+            }
+        }
+    }
+}
