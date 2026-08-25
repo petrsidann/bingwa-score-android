7454f9b3882fd317fc95e0227607b41848e896e0
petrsidann <petrsidann@gmail.com>
2026-08-25 06:48:22 +0300

    MainActivity.kt

--- app/src/main/java/com/bingwascore/app/ui/MainActivity.kt
+++ app/src/main/java/com/bingwascore/app/ui/MainActivity.kt
@@ -4,21 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import com.bingwascore.app.ui.navigation.BingwaNavHost
 import com.bingwascore.app.ui.theme.BingwaTheme
+import com.bingwascore.app.ui.theme.ThemeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val themeViewModel: ThemeViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            BingwaTheme {
+            val isDark by themeViewModel.isDark.collectAsState()
+            BingwaTheme(darkTheme = isDark) {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
