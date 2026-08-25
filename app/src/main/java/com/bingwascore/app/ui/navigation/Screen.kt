58c5f1829026b1f16545dc259f7ed6c21b7c5ada
petrsidann <petrsidann@gmail.com>
2026-08-25 06:50:44 +0300

    Screen.kt

--- app/src/main/java/com/bingwascore/app/ui/navigation/Screen.kt
+++ app/src/main/java/com/bingwascore/app/ui/navigation/Screen.kt
@@ -5,9 +5,15 @@ sealed class Screen(val route: String) {
     data object Login : Screen("login")
     data object Signup : Screen("signup")
     data object Home : Screen("home")
+    data object Offers : Screen("offers")
+    data object QuickDial : Screen("quickdial")
+    data object AutoRenewals : Screen("autorenewals")
+    data object Subscriptions : Screen("subscriptions")
+    data object AutoReplies : Screen("autoreplies")
+    data object Community : Screen("community")
+    data object Settings : Screen("settings")
     data object Orders : Screen("orders")
     data object Account : Screen("account")
-    data object Settings : Screen("settings")
     data object Admin : Screen("admin")
     data object Checkout : Screen("checkout/{bundleId}") {
         fun createRoute(bundleId: String) = "checkout/$bundleId"
