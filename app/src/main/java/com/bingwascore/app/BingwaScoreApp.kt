4b0932bde5762a7dd41900570c987133695721b9
petrsidann <petrsidann@gmail.com>
2026-08-25 05:31:41 +0300

    BingwaScoreApp.kt

--- app/src/main/java/com/bingwascore/app/BingwaScoreApp.kt
+++ app/src/main/java/com/bingwascore/app/BingwaScoreApp.kt
@@ -1,29 +1,17 @@
 package com.bingwascore.app
 
 import android.app.Application
-import androidx.work.Configuration
-import androidx.work.WorkManager
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
-import javax.inject.Inject
 
 @HiltAndroidApp
-class BingwaScoreApp : Application(), Configuration.Provider {
-
-    @Inject
-    lateinit var workManagerConfiguration: Configuration
-
-    override fun getWorkManagerConfiguration(): Configuration {
-        return workManagerConfiguration
-    }
+class BingwaScoreApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
-        
         Timber.d("Bingwa Score App initialized")
     }
 }
