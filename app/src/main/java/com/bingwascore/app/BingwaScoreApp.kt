0f36091e0c86206ea58f42b97a648dd7041d2972
petrsidann <petrsidann@gmail.com>
2026-08-26 16:45:07 +0300

    BingwaScoreApp.kt

--- app/src/main/java/com/bingwascore/app/BingwaScoreApp.kt
+++ app/src/main/java/com/bingwascore/app/BingwaScoreApp.kt
@@ -1,13 +1,25 @@
 package com.bingwascore.app
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import com.bingwascore.app.workers.AutoRenewalScheduler
 import com.bingwascore.app.workers.SmsPollScheduler
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
+import javax.inject.Inject
 
 @HiltAndroidApp
-class BingwaScoreApp : Application() {
+class BingwaScoreApp : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override fun getWorkManagerConfiguration(): Configuration {
+        return Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+    }
 
     override fun onCreate() {
         super.onCreate()
