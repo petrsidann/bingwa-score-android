c1ec20f1f441c3ae87d8bfca70178a33fc7200bb
petrsidann <petrsidann@gmail.com>
2026-08-27 20:22:20 +0300

    Constants.kt

--- app/src/main/java/com/bingwascore/app/util/Constants.kt
+++ app/src/main/java/com/bingwascore/app/util/Constants.kt
@@ -6,7 +6,5 @@ object Constants {
     const val KEY_REFRESH_TOKEN = "refresh_token"
     const val KEY_USER_ID = "user_id"
     const val KEY_THEME_MODE = "theme_mode"
-
-    const val PHONE_PREFIX = "+254"
-    const val MIN_PASSWORD_LENGTH = 6
+    const val BASE_URL = "https://api.bingwascore.com/"
 }
