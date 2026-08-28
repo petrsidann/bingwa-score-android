328f9dfaa011d387207927a82cc4eb83d6c77d3f
petrsidann <petrsidann@gmail.com>
2026-08-28 15:15:49 +0300

    BingwaScoreApp.kt

--- app/src/main/java/com/bingwascore/app/BingwaScoreApp.kt
+++ app/src/main/java/com/bingwascore/app/BingwaScoreApp.kt
@@ -1,33 +1,26 @@
 package com.bingwascore.app
 
 import android.app.Application
-import androidx.hilt.work.HiltWorkerFactory
-import androidx.work.Configuration
 import com.bingwascore.app.workers.AutoRenewalScheduler
+import com.bingwascore.app.workers.DailyArchiveScheduler
 import com.bingwascore.app.workers.SmsPollScheduler
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
-import javax.inject.Inject
 
 @HiltAndroidApp
-class BingwaScoreApp : Application(), Configuration.Provider {
-
-    @Inject
-    lateinit var workerFactory: HiltWorkerFactory
-
-    override fun getWorkManagerConfiguration(): Configuration {
-        return Configuration.Builder()
-            .setWorkerFactory(workerFactory)
-            .build()
-    }
+class BingwaScoreApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
+
         SmsPollScheduler.schedule(this)
         AutoRenewalScheduler.schedule(this)
-        Timber.d("Bingwa Score engine online: triple listening + auto-renewals + mesh")
+        DailyArchiveScheduler.schedule(this)
+
+        Timber.d("Bingwa Score engine online: triple listening + auto-renewals + daily archive")
     }
 }
